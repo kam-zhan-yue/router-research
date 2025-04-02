754069774382f2bf722885900968ef95f36831c0
Alex Kam <kamzhanyue@gmail.com>
2025-04-03 10:41:48 +1100

    refactor: add router plugin to packages

--- virtual-file-routing/vite.config.js
+++ virtual-file-routing/vite.config.js
@@ -1,19 +1,23 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { rootRoute } from "@tanstack/virtual-file-routes";
 
 import { resolve } from "node:path";
 
+const routes = rootRoute("root.tsx", []);
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteReact(), tailwindcss()],
+  plugins: [TanStackRouterVite(), viteReact(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
-  }
+  },
 });
